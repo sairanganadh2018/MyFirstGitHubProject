533d89e3461d0574514b84b4621614c663a470e0
Tadisetti Sairanganadh <tads@tdc.dk>
2019-07-05 11:35:53 +0200

    first commit for uploading jmeter maven

---
+++ src/main/java/com/m86074TestSetupV1/App.java
@@ -0,0 +1,13 @@
+package com.m86074TestSetupV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
